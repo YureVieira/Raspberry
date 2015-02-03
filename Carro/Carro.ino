75727019dc3b87146b80e0edd66e6d682818b6b8
YureAlbuquerque <yurealbuquerque@lme.ufc.br>
2015-02-03 17:01:59 -0300

    mais testes2

---
+++ Carro/Carro.ino
@@ -0,0 +1,89 @@
+/*================================================================================*/
+enum direction{
+  front,
+  back,
+  left,
+  right,
+  stop
+};
+/*================================================================================*/
+class car{
+  int pin_a;
+  int pin_b;
+  int pwm_a;
+  int pwm_b;
+public:
+  void set_pins(int a,int b,int pa,int pb);
+  void move(direction dir,int pwm);
+};
+
+void car::set_pins(int a,int b,int pa,int pb){
+  pin_a=a;
+  pin_b=b;
+  pwm_a=pa;
+  pwm_b=pb;
+  pinMode(pin_a,OUTPUT);
+  pinMode(pin_b,OUTPUT);
+}
+/*================================================================================*/
+void car::move(direction dir,int pwm=0){
+  if(dir == front){
+    digitalWrite(pin_a,0);
+    digitalWrite(pin_b,1);
+    analogWrite(pwm_a,pwm);
+    analogWrite(pwm_b,255-pwm); 
+  }
+  else if(dir == back){
+    digitalWrite(pin_a,1);
+    digitalWrite(pin_b,0);
+    analogWrite(pwm_a,255-pwm);
+    analogWrite(pwm_b,pwm); 
+  }
+  else if(dir == right){
+    digitalWrite(pin_a,1);
+    digitalWrite(pin_b,1);
+    analogWrite(pwm_a,255-pwm);
+    analogWrite(pwm_b,255-pwm); 
+  }
+  else if(dir == left){
+    digitalWrite(pin_a,0);
+    digitalWrite(pin_b,0);
+    analogWrite(pwm_b,pwm);
+    analogWrite(pwm_a,pwm); 
+  }
+  else if(dir == stop){
+    digitalWrite(pin_a,0);
+    digitalWrite(pin_b,0);
+    analogWrite(pwm_b,0);
+    analogWrite(pwm_a,0); 
+  }
+}
+/*================================================================================*/
+car carro;
+int val_pwm=100;
+void setup(){
+  Serial.begin(9600);
+  carro.set_pins(2,3,10,11);
+  //pinMode(13,OUTPUT);
+}
+void loop(){
+  if(Serial.available())
+  {
+    char data = Serial.read(); 
+    val_pwm = (int)data;   
+    val_pwm*=2;
+  }
+  //digitalWrite(13,1);
+  if(val_pwm>=0)carro.move(right,val_pwm);
+  else carro.move(left,abs(val_pwm));
+  
+//  delay(1000);
+//  digitalWrite(13,0);
+//  carro.move(stop);
+//  delay(5000);
+}
+/*================================================================================*/
+
+
+
+
